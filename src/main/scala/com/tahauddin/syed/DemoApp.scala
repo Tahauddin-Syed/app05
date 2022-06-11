4bd46bab58d1dc160884685de457720291e263ce
Tahauddin-Syed <tahauddin.syed@gmail.com>
2022-06-11 16:40:59 +0300

    Initial commit

---
+++ src/main/scala/com/tahauddin/syed/DemoApp.scala
@@ -0,0 +1,12 @@
+package com.tahauddin.syed
+
+/**
+ * extending predefined App Class,
+ * It already has main method, here we don't need to write explicitly main method
+ */
+object DemoApp extends App {
+
+
+  println("Hello World from predefine main method")
+
+}
---
+++ src/main/scala/com/tahauddin/syed/DemoAppOne.scala
@@ -0,0 +1,12 @@
+package com.tahauddin.syed
+
+/**
+ * DemoAppOne with main method
+ */
+object DemoAppOne {
+
+  def main(args: Array[String]): Unit = {
+    println("Hello World - from user defined main method")
+  }
+
+}
